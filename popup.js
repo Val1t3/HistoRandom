97d8280d08e152a5738ebbd7ea2f2fd2fee292e9
Val <valentinwoehrel2003@gmail.com>
2022-10-09 11:31:55 +0200

    [ADD] urls in popup.js

--- popup.js
+++ popup.js
@@ -1,3 +1,96 @@
+var urls = [
+    {
+        name: "Poumon",
+        url: "https://doc-pedagogie.umontpellier.fr/medecine/histologieLV/index.php?module=detail&vue=1&itm=20&lame=16&g=0&d=1"
+    }, {
+        name: "Rein",
+        url: "https://doc-pedagogie.umontpellier.fr/medecine/histologieLV/index.php?module=detail&vue=2&itm=62&lame=35&g=0&d=1"
+    }, {
+        name: "Thyroïde",
+        url: "https://doc-pedagogie.umontpellier.fr/medecine/histologieLV/index.php?module=detail&vue=2&itm=40&lame=4&g=0&d=1"
+    }, {
+        name: "Foie",
+        url: "https://doc-pedagogie.umontpellier.fr/medecine/histologieLV/index.php?module=detail&vue=1&itm=17&lame=20&g=0&d=1"
+    }, {
+        name: "Glandes surrénales",
+        url: "https://doc-pedagogie.umontpellier.fr/medecine/histologieLV/index.php?module=detail&vue=2&itm=47&lame=83&g=0&d=1"
+    }, {
+        name: "Pancréas",
+        url: "https://doc-pedagogie.umontpellier.fr/medecine/histologieLV/index.php?module=detail&vue=2&itm=56&lame=34&g=0&d=1"
+    }, {
+        name: "Parotide",
+        url: "https://doc-pedagogie.umontpellier.fr/medecine/histologieLV/index.php?module=detail&vue=1&itm=31&lame=28&g=0&d=1"
+    }, {
+        name: "Hypophyse",
+        url: "https://doc-pedagogie.umontpellier.fr/medecine/histologieLV/index.php?module=detail&vue=3&itm=80&lame=40&g=0&d=1"
+    }, {
+        name: "Parathyroïde",
+        url: "https://doc-pedagogie.umontpellier.fr/medecine/histologieLV/index.php?module=detail&vue=2&itm=57&lame=38&g=0&d=1"
+    }, {
+        name: "Glande sous maxillaire",
+        url: "https://doc-pedagogie.umontpellier.fr/medecine/histologieLV/index.php?module=detail&vue=1&itm=33&lame=49&g=0&d=1"
+    }, {
+        name: "Amygdale",
+        url: "https://doc-pedagogie.umontpellier.fr/medecine/histologieLV/index.php?module=detail&vue=3&itm=69&lame=54&g=0&d=1"
+    }, {
+        name: "Appendice",
+        url: "https://doc-pedagogie.umontpellier.fr/medecine/histologieLV/index.php?module=detail&vue=1&itm=3&lame=13&g=0&d=1"
+    }, {
+        name: "Ganglion",
+        url: "https://doc-pedagogie.umontpellier.fr/medecine/histologieLV/index.php?module=detail&vue=3&itm=78&lame=70&g=0&d=1"
+    }, {
+        name: "Rate",
+        url: "https://doc-pedagogie.umontpellier.fr/medecine/histologieLV/index.php?module=detail&vue=3&itm=92&lame=44&g=0&d=1"
+    }, {
+        name: "Moelle osseuse",
+        url: "https://doc-pedagogie.umontpellier.fr/medecine/histologieLV/index.php?module=detail&vue=3&itm=82&lame=56&g=0&d=1"
+    }, {
+        name: "Thymus",
+        url: "https://doc-pedagogie.umontpellier.fr/medecine/histologieLV/index.php?module=detail&vue=3&itm=94&lame=43&g=0&d=1"
+    }, {
+        name: "Oesophage",
+        url: "https://doc-pedagogie.umontpellier.fr/medecine/histologieLV/index.php?module=detail&vue=1&itm=55&lame=23&g=0&d=1"
+    }, {
+        name: "Duodénum",
+        url: "https://doc-pedagogie.umontpellier.fr/medecine/histologieLV/index.php?module=detail&vue=2&itm=51&lame=12&g=0&d=1"
+    }, {
+        name: "Jejunum-Ileon",
+        url: "https://doc-pedagogie.umontpellier.fr/medecine/histologieLV/index.php?module=detail&vue=1&itm=1&lame=9&g=0&d=1"
+    }, {
+        name: "Côlon",
+        url: "https://doc-pedagogie.umontpellier.fr/medecine/histologieLV/index.php?module=detail&vue=1&itm=9&lame=1&g=0"
+    }, {
+        name: "Estomac région fundique",
+        url: "https://doc-pedagogie.umontpellier.fr/medecine/histologieLV/index.php?module=detail&vue=1&itm=16&lame=25&g=0"
+    }, {
+        name: "Estomac région pylorique",
+        url: "https://doc-pedagogie.umontpellier.fr/medecine/histologieLV/index.php?module=detail&vue=1&itm=16&lame=111&g=0"
+    }, {
+        name: "Vésicule biliaire",
+        url: "https://doc-pedagogie.umontpellier.fr/medecine/histologieLV/index.php?module=detail&vue=2&itm=66&lame=36&g=0"
+    }, {
+        name: "Trachée",
+        url: "https://doc-pedagogie.umontpellier.fr/medecine/histologieLV/index.php?module=detail&vue=2&itm=52&lame=39&g=0"
+    }, {
+        name: "Bronches",
+        url: "https://doc-pedagogie.umontpellier.fr/medecine/histologieLV/index.php?module=detail&vue=2&itm=26&lame=17&g=0"
+    }, {
+        name: "Bronchioles",
+        url: "https://doc-pedagogie.umontpellier.fr/medecine/histologieLV/index.php?module=detail&subaction=lame&lame=85&vue=2&itm=46&g=0&d=0"
+    }, {
+        name: "Artère musculaire",
+        url: "https://doc-pedagogie.umontpellier.fr/medecine/histologieLV/index.php?module=detail&vue=1&itm=5&lame=79&g=0"
+    }, {
+        name: "Artère aorte",
+        url: "https://doc-pedagogie.umontpellier.fr/medecine/histologieLV/index.php?module=detail&vue=1&itm=2&lame=63&g=0&d=1"
+    }
+]
+
+function RandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
 document.addEventListener('DOMContentLoaded', () => {
     var answerButton = document.getElementById('answer');
     var newButton = document.getElementById('new');
@@ -7,6 +100,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }, false);
 
     newButton.addEventListener('click', () => {
-        chrome.tabs.update(null, {url:"https://www.youtube.com"});
+
+        chrome.tabs.update(null, { url: urls[RandomNumber(0, 27)].url });
     }, false);
 }, false);
